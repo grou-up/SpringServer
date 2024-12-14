c7f7c143b13b57b819b63e35950e26fa3f6362e2
boseok <fa7271@naver.com>
2024-12-14 11:48:38 +0900

    [feat] entity erd 대로 일단 단방향 추가 필요할때 양방향 설정 아직 컨트롤러 서비스 레포 없어서 임시폴더 tempdomain에서 관리

---
+++ src/main/java/growup/spring/springserver/tempdomain/CampaignOptionDetails.java
@@ -0,0 +1,43 @@
+package growup.spring.springserver.tempdomain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+
+@EntityListeners(AuditingEntityListener.class)
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@Getter
+@AllArgsConstructor
+public class CampaignOptionDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDate copDate;  // 날짜
+
+    private Long copImpressions;  // 노출수
+
+    private Long copSales;  // 주문수
+
+    private Double copAdcost;  // 광고비
+
+    private Double copAdsales;  // 광고매출
+
+    private Double copRoas;  // ROAS
+
+    private Long copClicks;  // 클릭수
+
+    private Double copClickRate;  // 클릭률
+
+    private Double copCvr;  // 전환율
+
+    private String copSearchType;  // 검색 비검색
+
+    @ManyToOne
+    @JoinColumn(name = "execution_id", referencedColumnName = "execution_id")
+    private Execution execution;
+}
