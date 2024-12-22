42bccc0d1b391eaf3aebc89960db70f918698739
Hi-Imjaeyoung <imjy2757@naver.com>
2024-12-22 16:38:48 +0900

    refactor : API URL 공통화.

--- src/main/java/growup/spring/springserver/campaign/CampaignController.java
+++ src/main/java/growup/spring/springserver/campaign/CampaignController.java
@@ -1,24 +1,35 @@
 package growup.spring.springserver.campaign;
 
+import growup.spring.springserver.global.common.CommonResponse;
+import growup.spring.springserver.login.dto.response.LoginResDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @Slf4j
-@Controller
+@RequestMapping("/api/campaign")
+@RestController
 public class CampaignController {
     @Autowired
     private CampaignService campaignService;
 
-    @GetMapping("/campaign/getMyCampaigns")
-    public String getMyCampaigns(@AuthenticationPrincipal UserDetails userDetails) {
+    @GetMapping("/getMyCampaigns")
+    public ResponseEntity<CommonResponse<?>>getMyCampaigns(@AuthenticationPrincipal UserDetails userDetails) {
+        log.info("Start getMyCampaigns API target is :"+userDetails.getUsername());
         List<String> data = campaignService.getMyCampaigns(userDetails.getUsername());
-        // 추가 로직 수행
-        return "OK";
+        log.info("End getMyCampaigns API target is :"+userDetails.getUsername());
+        return new ResponseEntity<>(CommonResponse
+                .<List<String>>builder("success : load campaign name list")
+                .data(data)
+                .build(), HttpStatus.OK);
     }
 }
--- src/test/java/growup/spring/springserver/campaign/CampaignControllerTest.java
+++ src/test/java/growup/spring/springserver/campaign/CampaignControllerTest.java
@@ -1,29 +1,30 @@
 package growup.spring.springserver.campaign;
 
+import com.nimbusds.jose.shaded.gson.Gson;
 import growup.spring.springserver.annotation.WithAuthUser;
 import growup.spring.springserver.global.config.JwtTokenProvider;
-import growup.spring.springserver.global.fillter.JwtAuthFilter;
-import jakarta.servlet.http.HttpServletRequest;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.mockito.Mockito.doReturn;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(MockitoExtension.class)
 @WebMvcTest(CampaignController.class)
@@ -41,19 +42,30 @@ public class CampaignControllerTest {
     @Test
     @DisplayName("getMyCampaigns : ErrorCase1.인가되지 않은 사용자 접근")
     void test1() throws Exception {
-        final String url = "/campaign/getMyCampaigns";
+        final String url = "/api/campaign/getMyCampaigns";
         final ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.get(url));
-        resultActions.andDo(print()).andExpect(MockMvcResultMatchers.status().isUnauthorized());
+        resultActions.andDo(print()).andExpect(status().isUnauthorized());
     }
 
     @Test
     @DisplayName("getMyCampaigns : Success1.접근 성공")
     @WithAuthUser
     void test2() throws Exception {
-        final String url = "/campaign/getMyCampaigns";
-        doReturn(List.of()).when(campaignService).getMyCampaigns("test@test.com");
-        final ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.get(url));
-        resultActions.andDo(print()).andExpect(MockMvcResultMatchers.status().isOk());
-    }
+        Gson gson = new Gson();
+        final String url = "/api/campaign/getMyCampaigns";
+        doReturn(List.of("name1","name2","name3")).when(campaignService).getMyCampaigns("test@test.com");
+        final ResultActions resultActions = mockMvc.perform(
+                MockMvcRequestBuilders.get(url)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(gson.toJson(new ArrayList<String>())));
 
+        resultActions.andExpectAll(
+                status().isOk(),
+                jsonPath("message").value("success : load campaign name list"),
+                jsonPath("data").isArray(), // data가 배열인지 확인
+                jsonPath("data[0]").value("name1"), // 첫 번째 요소 검증
+                jsonPath("data[1]").value("name2"), // 두 번째 요소 검증
+                jsonPath("data[2]").value("name3")  // 세 번째 요소 검증
+        ).andDo(print());
+    }
 }
