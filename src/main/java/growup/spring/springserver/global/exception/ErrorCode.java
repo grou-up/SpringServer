6139beb9cf63775a37036323f5bc790b04b184fc
boseok <fa7271@naver.com>
2025-01-06 13:02:04 +0900

    [feat] ErrorCode 틀 변경,

---
+++ src/main/java/growup/spring/springserver/global/exception/ErrorCode.java
@@ -0,0 +1,28 @@
+package growup.spring.springserver.global.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+import java.util.Optional;
+import java.util.function.Predicate;
+
+@Getter
+@RequiredArgsConstructor
+public enum ErrorCode {
+
+    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 회원입니다."),
+    MEMBER_KAKAO_LOGIN_FOUND(HttpStatus.BAD_REQUEST,"해당 계정은 카카오 로그인 전용입니다."),
+    MemberInvalidPasswordException(HttpStatus.BAD_REQUEST, "비밀번호가 틀렸습니다."),
+    ;
+
+
+    ErrorCode(HttpStatus httpStatus, String message){
+        this.httpStatus = httpStatus;
+        this.message = message;
+    }
+
+    private HttpStatus httpStatus;
+    private String message;
+
+}
