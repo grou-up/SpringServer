e23721e7f82e95321312e454acbf20393465089c
boseok <fa7271@naver.com>
2025-01-27 16:02:12 +0900

    [feat/margin_Firstpage_Calculate] 멤버와 1:N 으로 변경 및 상품명, 순  팜내수, 순 매출액

--- src/main/java/growup/spring/springserver/netsales/domain/NetSales.java
+++ src/main/java/growup/spring/springserver/netsales/domain/NetSales.java
@@ -1,10 +1,12 @@
 package growup.spring.springserver.netsales.domain;
 
 import growup.spring.springserver.campaign.domain.Campaign;
-import growup.spring.springserver.marginforcampaign.domain.MarginForCampaign;
+import growup.spring.springserver.login.domain.Member;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
 
 @EntityListeners(AuditingEntityListener.class)
 @Entity
@@ -16,10 +18,12 @@ public class NetSales {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    private Long netSalesCount; // 순수판매수
-
+    private String netProductName;
+    private Long netSalesAmount; // 순 판매금액
+    private Long netSalesCount; // 순 판매수
+    private LocalDate netDate;
     @ManyToOne
-    @JoinColumn(name = "mfc_id", referencedColumnName = "mfc_id")
-    private MarginForCampaign marginForCampaign;
+    @JoinColumn(name = "email", referencedColumnName = "email")
+    private Member member;
+
 }
