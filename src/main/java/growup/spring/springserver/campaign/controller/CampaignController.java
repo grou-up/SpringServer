6e01dbc47a5d84abc542f17766cc55d07d50b5e2
Hi-Imjaeyoung <imjy2757@naver.com>
2025-02-05 17:25:07 +0900

    test : test url 생성.

--- src/main/java/growup/spring/springserver/campaign/controller/CampaignController.java
+++ src/main/java/growup/spring/springserver/campaign/controller/CampaignController.java
@@ -32,9 +32,4 @@ public class CampaignController {
                 .data(data)
                 .build(), HttpStatus.OK);
     }
-
-    @GetMapping("/test")
-    public String check() {
-        return "OK";
-    }
 }
