34f9cad19db2a77fb59fc911abe927726e117e28
boseok <fa7271@naver.com>
2025-02-20 00:10:53 +0900

    [feat] 구글 로그인 구현 및 팩토리 메서드로, 인터페이스로 구

---
+++ src/main/java/growup/spring/springserver/global/oauth/OAuth2RedirectUrlProvider.java
@@ -0,0 +1,23 @@
+package growup.spring.springserver.global.oauth;
+
+import lombok.experimental.UtilityClass;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@UtilityClass
+public class OAuth2RedirectUrlProvider {
+    private static final Map<String, String> REDIRECT_URLS = new HashMap<>();
+
+    static {
+        REDIRECT_URLS.put("kakao", "http://localhost:3000/oauth/kakao/callback?token=%s");
+        REDIRECT_URLS.put("google", "http://localhost:3000/oauth/google/callback?token=%s");
+    }
+
+    public static String getRedirectUrl(String registrationId, String accessToken) {
+        if (!REDIRECT_URLS.containsKey(registrationId)) {
+            throw new IllegalArgumentException("지원하지 않는 OAuth 공급자: " + registrationId);
+        }
+        return String.format(REDIRECT_URLS.get(registrationId), accessToken);
+    }
+}
